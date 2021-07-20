9b0c6715dd7bd2df503391bf6abe5a1ee37d529f
Francisco Campos <f.campos.gqs@gmail.com>
2021-07-20 23:32:43 +0100

    building POS feature

--- resources/views/POS/add_customer.blade.php
+++ resources/views/POS/add_customer.blade.php
@@ -3,29 +3,29 @@
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                <h5 class="modal-title" id="exampleModalLabel">Selecione o Cliente</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
             <div class="modal-body">
-                <div class="col-md-12 form-group">
-                    <input type="search" id="first-name" class="form-control form-control-lg" name="fname" placeholder="NIF ou Nome do Cliente" wire:model.debounce.300ms="customerSearch">
-                    <ul class="list-group">
-                        @if(strlen($customerSearch) < 3)
-                        @else
+                <div class="list-group">
+                    <input type="search" class="list-group-item list-group-item-action" placeholder="NIF ou Nome do Cliente" wire:model.debounce.300ms="customerSearch">
+
+                    @if(strlen($customerSearch) < 3)
+                    @else
                         @forelse($searchResults as $result)
-                            <li class="list-group-item">
+                            <a href="#" class="list-group-item list-group-item-action">
                                 {{$result['name']}}
-                            </li>
+                            </a>
                         @empty
-                                <li class="list-group-item">
-                                    Sem resultados para: "{{$customerSeach}}"
-                                </li>
+                            <li class="list-group-item">
+                                Sem resultados para: "{{$customerSearch}}"
+                            </li>
                         @endforelse
-                        @endif
-                    </ul>
+                    @endif
                 </div>
+
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
